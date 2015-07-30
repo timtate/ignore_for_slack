8b4e4a92cde13ad8da1ed223ceef24cf12e31b0f
Tim Tate <tim@parthenonsoftware.com>
2015-07-30 14:14:25 -0700

    added some basic instructions and started ignoring in the DM list too

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ignore_for_slack
-Javascript to ignore a user in Slack (in-browser only)
+# Ignore for Slack
+In-browser script to ignore a user in Slack
+This will not work in the desktop app, or the mobile app, but if you run Slack in your browser, you might find it useful.
+This will not block a user across Slack teams, so if the person is in more than one team that you're also in, you'll have to apply this for each team.
+
+To use:
+Right-click on the user icon of the user you'd like to ignore, and select "Inspect Element".
+In the Element Inspector, look for an element with the data-member-id attribute. It will start with a U, like U03CGXYTN.
+Replace the member_id value the script with this ID.
+Run the script when you visit your Slack team (more specific instructions to come)
--- ignore.js
+++ ignore.js
@@ -1,7 +1,11 @@
-var member_id = "U02BGQYTN"; //this is my ID, replace with the member_id of the user you'd like to ignore
-$('a[data-member-id="'+member_id+'"]').parent().hide();
-var observer = new MutationObserver(function(mutations, observer) {
+var member_id = "U03CGHYTN"; //this is a fake ID, replace with the member_id of the user you'd like to ignore
+
+$('a[data-member-id="'+member_id+'"]').parent().hide(); // this hides all currently displayed messages by that user
+$('li.member_'+member_id).hide(); // and their listing in the direct messages list
+
+var observer = new MutationObserver(function(mutations, observer) { // this watches for new messages and appearances by that user
     $('a[data-member-id="'+member_id+'"]').parent().hide();
+    $('li.member_'+member_id).hide();
 });
 
 observer.observe(document, {
